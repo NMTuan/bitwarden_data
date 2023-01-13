e5f49d8df9cae3957e264be0310d6ead9527242c
nmtuan <nmtuan@qq.com>
2023-01-13 15:40:14 +0800

    perf: 完善顶部 guide 超链接

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2023-01-08 17:27:54
- * @LastEditTime: 2023-01-12 19:54:48
+ * @LastEditTime: 2023-01-13 15:34:58
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \bitwarden_data_de_duplication\src\views\HomeView.vue
@@ -37,7 +37,7 @@
       <li class="done">重复数据的处理</li>
       <li class="done">浏览器数据缓存，方便下次继续处理</li>
       <li class="done">数据的列表展示</li>
-      <li>数据编辑与删除</li>
+      <li class="done">数据编辑与删除</li>
     </ul>
 
     <h2>## 使用</h2>
